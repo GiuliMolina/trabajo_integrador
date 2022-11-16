63b670cc2fb332d4009b9ecdeb84bc72d8f84526
Giuliana Molina <gmolina@udesa.edu.ar>
2022-11-16 20:15:56 -0300

    Arregle detalles

--- js/detallepelis.js
+++ js/detallepelis.js
@@ -35,12 +35,13 @@ fetch(detallepelis+apiKey)
     console.log(data)
 
         info+=` <section class="title">
-                  <h2>${data.original_title}</h2>
                   <article class="imagen">
+                    <h2>${data.original_title}</h2>
+                  
                     <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}" height="500px" width="300px">
                   </article>
-                  </section>
-                  <section class="section1">
+                </section>
+                <section class="section1">
                   <article class="article1">
                     <h4> ESTRENO: </h4>
                     <p> ${data.release_date}</p>
@@ -55,12 +56,10 @@ fetch(detallepelis+apiKey)
                     for (let i=0; i<data.genres.length; i++){
                     let genero= data.genres[i].name
                     let idgenero= data.genres[i].id
-                info+=(`
-                    <a class="generos" href="./generos.html?id=${idgenero}"> ${genero}</a>
-                </article>
-                `)
-                }
-                info+=`
+                    info+=(`
+                        <a class="generos" href="./generos.html?id=${idgenero}"> ${genero}</a></article> `)}
+                        
+                  info+=`
                   <article class="article1">
                     <h4> RATING: </h5>
                     <p> rating¿? </p>
@@ -68,7 +67,7 @@ fetch(detallepelis+apiKey)
                   <article class="article1">
                     <h4> VER EN: </h5>
                     <p> donde¿? </p>
-                    <button class='favoritos'> ${text} </button>  
+                    
                   </article>
                   <article class="article2">
                     <p>${data.overview}</p>
@@ -78,25 +77,25 @@ fetch(detallepelis+apiKey)
          `
   ///BOTON FAVORITOS///
   
-  let favoritos = obtenerfav()
-  let elementoFav = favoritos.includes(data.idFav)
-  let text = ''
-  if (elementoFav){
-    text = 'Eliminar de favoritos'
-  }else{
-    text = 'Agregar a favoritos'
-  }
-  /*contenedor.innerHTML = `
+      let favoritos = obtenerfav()
+      let elementoFav = favoritos.includes(data.idFav)
+      let text = ''
+      if (elementoFav){
+        text = 'Eliminar de favoritos'
+      }else{
+        text = 'Agregar a favoritos'
+      }
+  contenedor.innerHTML = `
   <article>
     <h5> ${data.title}</h5>
     <img src=${data.poster_path}
     <button class='favoritos'> ${text} </button>
-  </article>`*/
+  </article>`
   
-  let botonfavs = document.querySelector('.favoritos')
-  console.log(botonfavs)
+    let botonfavs = document.querySelector('.favoritos')
+    
 
-  botonfavs.addEventListener('click', function(e){
+    botonfavs.addEventListener('click', function(e){
     let favoritos = obtenerfav()
     let elementoFav = favoritos.includes(data.idFav)
     if(elementoFav){
@@ -109,9 +108,10 @@ fetch(detallepelis+apiKey)
   })
 
     contenedor.innerHTML= info
-  })
+})
+
 .catch(function(error){
-    console.log(`El error es ${error}`)
+  console.log(error)
 })
 
 
