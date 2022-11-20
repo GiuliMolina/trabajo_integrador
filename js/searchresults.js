e40cbe0a74d2fa048de96343b006b7af6f2a20f4
unknown <chelmich@udesa.edu.ar>
2022-11-20 11:35:28 -0300

    no esta terminado search results

--- js/searchresults.js
+++ js/searchresults.js
@@ -1,157 +1,41 @@
-window.addEventListener('load', function () {
-  let cargaPrevia = document.querySelector('.spinCarga');
-  // cargaPrevia.style.display = "none"
-})
+let sT = location.search
+let QsT = new UrlSearchParams(sT)
+let buscado = QsT.get('busquedor')
 
-// Resultados de Películas
+let UrlSearch = 'https://api.themoviedb.org/3/movie/popular?api_key=0c5fb97f0c55576b638b49d73fa8d73e'
 
-let qs1 = location.search; 
-let qsto1 = new URLSearchParams(qs1);
-let queryP = qsto1.get('busquedor'); 
+fetch(UrlSearch)
+.then(function(res){
+  return res.json();
 
-console.log(qs1);
-
-let tituloPrincipal = document.querySelector('.resultados1');
-tituloPrincipal.innerText = `El término buscado es: ${queryP}`
-let id = objDetalle.get("id")
-
-
-let info = ''
-
-fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0c5fb97f0c55576b638b49d73fa8d73e`)
-.then(function(response){
-  return response.json();
 })
 .then(function(data){
   console.log(data);
+  let result = document.querySelector('.resultados1');
+  let search = document.querySelector('.resultados');
 
-  if (infoSearchMovies.length == 0) {
-      let moviesNotFound = document.querySelector('.moviesNotFound')
-      moviesNotFound.innerText = `No se han encontrado películas que coincidan con: ${queryP}`
-  } else {
-
-      for(let i=0; i<5; i++) {
-               info +=                          `<section class="title">
-                                                  <h2>${data.original_title}</h2>
-                                                </section>
-
-                                                <article class="imagen">
-                                                  <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}" height="500px" width="300px">
-                                                </article>
-
-                                                <section class="section1">
-                                                <article class="article1">
-                                                  <h4> ESTRENO: </h4>
-                                                  <p> ${data.release_date}</p>
-                                                </article>
-                                                <article class="article1">
-                                                  <h4> DURACIÓN: </h5>
-                                                  <p>${data.runtime} minutos </p>
-                                                </article>
-                                                <article class="article1">
-                                                <h4> GÉNEROS:</h5>`
-                                                for (let i=0; i<data.genres.length; i++){
-                                                  let genero= data.genres[i].name
-                                                  let idgenero= data.genres[i].id
-                                                  info+=(`
-                                                      <a class="generos" href="./generos.html?id=${idgenero}"> ${genero}</a></article> `)}
-                                                      
-                                                info+=`
-                                                <article class="article1">
-                                                  <h4> RATING: </h4>
-                                                  <p> ${data.vote_average}</p>
-                                                </article>
-                                                <article class="article2">
-                                                  <p>${data.overview}</p>
-                                                </article>`
-              console.log(infoSearchMovies[i].poster_path);
-
-          
-      }
+  if (data.data.lenght == 0){
+    result.innerHTML = `No se han encontrados resultados de: ${buscado}`
+  }else{
+    result.innerHTML = `Resultado de busqueda de: ${buscado}`
   }
-      peliculasEncontradas.innerHTML = articulosPeliculasEncontradas;
 
-
-})
-
-.catch(function(error){
-  console.log(error)
-})
-
-
-
-// Resultado de series
-
-let qs2 = location.search
-let qsto2 = new URLSearchParams(qs2);
-let queryS = qsto2.get('busquedor'); 
-
-let detalleseries= `https://api.themoviedb.org/3/tv/${id}`
-let apiKey2 = '?api_key=0c5fb97f0c55576b638b49d73fa8d73e';
-
-fetch(detalleseries+apiKey2)
-.then(function(response){
-  return response.json();
-})
-.then(function(data){
-  console.log(data);
-  let infoSearchSeries = data.results;
-  let articulosSeriesEncontradas = '';
-  let seriesEncontradas = document.querySelector('.seriesEncontradas')
-
-  if (infoSearchSeries.length == 0) {
-      let seriesNotFound = document.querySelector('.seriesNotFound')
-      seriesNotFound.innerText = `No se han encontrado series que coincidan con: ${queryS}`
-  } else {
-      for (let i=0; i<5; i++) {
-      articulosSeriesEncontradas += ``
+  for (let i = 0 ; i < 5 ; i++){
+    search.innerHTML += `
+    <section class='section1'>
+        <article>
+          <h5><a class= 'titulopeli' href="./detallepelis.html?id=${data.results[i].id}"> ${data.results[i].original_title} </a></h5>
+          <a class= 'titulopeli' href="./detallepelis.html?id=${data.results[i].id}"><img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="${data.results[i].original_title}"></a>
+          <p><a class = 'textopeli' href="./detallepelis.html?id=${data.results[i].id}">${data.results[i].release_date}</a></p>
+          <nav class="navseries">
+            <a class="vermás" href="./detallepelis.html?id=${data.results[i].id}" > VER MÁS </a> 
+          </nav> 
+        </article>
+      </section>`
+  }
   
-  seriesEncontradas.innerHTML = articulosSeriesEncontradas;
-} }
 })
 
-
-.catch(function(error){
-  console.log(error);
+.then(function(error){
+  console.log('El error es' + error)
 })
-
-// formulario de búsqueda 
-let formulario = document.querySelector('form');
-let inputField = document.querySelector('.search');
-let message = document.querySelector('.message');
-
-formulario.addEventListener('submit', function(evento){
-evento.preventDefault();
-console.log('No se envió')
-
-if(inputField.value == ""){
-  message.innerText = "No has ingresado ningún término";
-} else if (inputField.value.length < 3) {
-  message.innerText = "Debes ingresar al menos 3 caracteres"
-} else {
-  this.submit();
-}
-})
-
-// validacion formulario de búsqueda mobile
-let formularioM = document.querySelector('.formBusquedaMobile');
-let inputFieldM = document.querySelector('.searchM');
-let messageM = document.querySelector('.messageM');
-
-formularioM.addEventListener('submit', function(evento){
-  evento.preventDefault();
-  console.log('No se envió')
-
-if(inputFieldM.value == ""){
-  messageM.innerText = "No has ingresado ningún término, ingrese de nuevo por favor";
-  messageM.style.textAlign= "center"
-} else if (inputFieldM.value.length < 2) {
-  messageM.innerText = "Debes ingresar al menos 2 caracteres"
-  messageM.style.textAlign= "center"
-} else {
-  this.submit();
-}
-})
-
-
-
